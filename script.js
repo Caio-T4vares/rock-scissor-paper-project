e23922de8f0cc93bdb56627880f0a4b217b9115a
Caio <102376017+Caio-T4vares@users.noreply.github.com>
2023-11-30 15:17:09 -0300

    Refatorar o sistema, adicionar UI e mostrar resultado

--- script.js
+++ script.js
@@ -5,49 +5,61 @@ function getComputerChoice() {
 function randomChoice() {
   return Math.floor(Math.random() * 3);
 }
+let computerScore = 0;
+let playerScore = 0;
 function playRound(computerChoice, playerChoice) {
   switch (playerChoice) {
     case "rock":
-      if (computerChoice == "rock") {
-        return "Draw";
-      } else if (computerChoice == "paper") {
-        return "Defeat";
-      } else {
-        return "Win";
-      }
-    case "paper":
       if (computerChoice == "paper") {
-        return "Draw";
-      } else if (computerChoice == "scissor") {
-        return "Defeat";
+        computerScore++;
       } else {
-        return "Win";
+        playerScore++;
       }
-    case "scissor":
+      break;
+    case "paper":
       if (computerChoice == "scissor") {
-        return "Draw";
-      } else if (computerChoice == "rock") {
-        return "Defeat";
+        computerScore++;
       } else {
-        return "Win";
+        playerScore++;
+      }
+      break;
+    case "scissor":
+      if (computerChoice == "rock") {
+        computerScore++;
+      } else {
+        playerScore++;
       }
   }
 }
-function game() {
-  let playerWins = 0;
-  let computerWins = 0;
-  for (let i = 0; i < 5; i++) {
-    let computerChoice = getComputerChoice();
-    let playerChoice = prompt("Choose your pick").toLowerCase();
-    let result = playRound(computerChoice, playerChoice);
-    if (result === "Defeat") {
-      computerWins++;
-    } else if (result == "Win") {
-      playerWins++;
-    }
+const buttons = document.querySelectorAll(".container button");
+let playerChoice = "";
+buttons.forEach((btn) => {
+  btn.addEventListener("click", function (event) {
+    buttons.forEach((selectedBtn) => {
+      if (playerChoice == selectedBtn.value) {
+        selectedBtn.style.borderColor = "black";
+      }
+    });
+    btn.style.borderColor = "gold";
+    playerChoice = btn.value;
+  });
+});
+
+const playerScoreNode = document.querySelector(".playerScore");
+const computerScoreNode = document.querySelector(".computerScore");
+const playBtn = document.querySelector("#playBtn");
+playBtn.addEventListener("click", function (event) {
+  let result = playRound(getComputerChoice(), playerChoice);
+  if (playerScore == 5) {
+    alert("Player wins!");
+    computerScore = 0;
+    playerScore = 0;
   }
-  if (playerWins > computerWins) alert("Congratulations, you are the winner!");
-  else if (playerWins < computerWins) alert("OH NO! You lost the game!");
-  else alert("Draw!!");
-}
-game();
+  if (computerScore == 5) {
+    alert("Computer wins!");
+    playerScore = 0;
+    computerScore = 0;
+  }
+  playerScoreNode.textContent = `${playerScore}`;
+  computerScoreNode.textContent = `${computerScore}`;
+});
